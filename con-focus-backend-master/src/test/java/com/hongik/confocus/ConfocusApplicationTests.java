6226a661156ebe097c481fe5dbce25af33e68cc8
Hong Hyun Seung <harryhong100@naver.com>
2021-04-28 20:58:09 +0900

    Initial commit

---
+++ con-focus-backend-master/src/test/java/com/hongik/confocus/ConfocusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hongik.confocus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfocusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
